d4a66113486f1e5b6e4276f21a2e6bbc2d4752d1
Don <101531305+don-yoon@users.noreply.github.com>
2022-08-17 13:56:08 -0700

    refactor complete! for now...

---
+++ src/components/ColorFunctions.js
@@ -0,0 +1,11 @@
+function colorChange (type) {
+  return "bg-" + type
+}
+function colorChangeBG (type) {
+  return "from-" + type
+}
+function colorChangeBG2 (type) {
+  return "to-" + type
+}
+
+export { colorChange, colorChangeBG, colorChangeBG2 }
