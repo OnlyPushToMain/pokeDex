a7a398933d610636aaac196cd1ccfa6ea2f2cfc2
stephen omarrah <stephenomarrah@gmail.com>
2022-12-09 11:43:19 -0600

    finished wtp game and responsiveness for mobile

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,13 @@ export default function Home ({ setQuery, query, filterParam, setFilterParam, po
   return (
     <>
       <SearchBar changeQuery={setQuery} query={query} filterParam={filterParam} setFilterParam={setFilterParam}/>
-      <div className="h-screen flex">
-        <div className="ml-[.5rem] flex-1 flex overflow-hidden ">
+      <div className="h-screen flex flex-col xsm:flex-row">
+        <div className="padding-[2px] flex-1 flex overflow-hidden xsm:w-[100%] xsm:ml-[0px] xsm:z-index:1 mb-[120px]">
           <div className="flex-1 overflow-y-scroll">
             <Index query={query} filterParam={filterParam} setFilterParam={setFilterParam} changeQuery={setQuery} pokemon={pokeData} loading={loading} showPoke={showPokemon} colors={colors} colorsBG={colorsBG} colorsBG2={colorsBG2}/>
           </div>
         </div>
-        <div className=" mr-2 mt-2 sticky top-0 left-0 right-0 overflow-hidden">
+        <div className=" xsm:mr-2 mt-2 sticky top-0 left-0 right-0 overflow-hidden">
           { loading2 ? <Card pokemon2={showPoke} loading2={loading2} colors={colors} colorsBG={colorsBG} colorsBG2={colorsBG2}/> : null}
         </div>
       </div>
